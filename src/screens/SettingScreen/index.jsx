3a3f3ca6e3c697593b91985266ea04d90a0a9dd5
TabarakCodeCraft <tabarak.ali1801a@sc.uobaghdad.edu.iq>
2024-07-09 11:21:01 +0300

    update

---
+++ src/screens/SettingScreen/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./style.css";
+
+const SettingsScreen = () => {
+    return (
+        <div className="card-settings">
+       
+        </div>
+    );
+}
+
+export default SettingsScreen;
