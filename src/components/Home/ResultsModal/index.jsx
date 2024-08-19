d751383841af4f7fabf261b3d2670ea7812f1b9a
Baker Alazzawi <baker.alazzawi0@gmail.com>
2024-08-19 12:04:41 +0300

    Update index.jsx

--- src/components/Home/ResultsModal/index.jsx
+++ src/components/Home/ResultsModal/index.jsx
@@ -128,7 +128,7 @@ export const ResultsModal = () => {
     }).then(({ err }) => {
       if (err) message.error("Error !");
       else {
-        message.success("Save Succefful.");
+        message.success(t("savedSuccessfully"));
         setRecord(null);
         setIsResultsModal(false);
         setIsReload(!isReload);
