763f94476fa1da7d39f10bd66bc76c588af21d45
Kadhim Salah Kadhim <ka02zm45@gmail.com>
2024-08-20 10:18:07 +0300

    is online

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -4,7 +4,7 @@ import dayjs from "dayjs";
 import { useAppStore } from "../libs/appStore";
 
 const useLogin = () => {
-  const { isLogin, setIsLogin, setUser } = useAppStore();
+  const { isLogin, setIsLogin, setUser, isOnline } = useAppStore();
 
   useEffect(() => {
     const userString = localStorage.getItem("lab-user");
@@ -33,77 +33,79 @@ const useLogin = () => {
   }, []);
 
   const checkExpire = async (user) => {
-    try {
-      const serialId = localStorage.getItem("lab-serial-id");
-      const exp = localStorage.getItem("lab-exp");
-      const labCreated = localStorage.getItem("lab-created");
-
-      if (!exp || !labCreated || !serialId) {
-        console.warn("Required data missing for expiration check.");
-        setIsLogin(false);
-        setUser(null);
-        return;
-      }
-
-      const createdDate = dayjs(labCreated);
-      const today = dayjs();
-      const dayPassed = today.diff(createdDate, "day");
-
-      // Define the expiration period based on the user type
-      const expirationPeriod = user?.type === "trial" ? 7 : 365;
-      const remaining = expirationPeriod - dayPassed;
-
-
-
-      if (remaining <= 0) {
-        setIsLogin(false);
-        setUser(null);
-        message.error("Subscription has expired.");
-        return;
-      }
-
-      if (user?.type !== "trial") {
-        const response = await fetch(
-          "https://dr-lab-apiv2.onrender.com/api/app/check-serial-expiration",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ serialId: parseInt(serialId, 10) }),
-          }
-        );
-
-        if (!response.ok) {
-          throw new Error(
-            `Failed to check serial expiration, status code: ${response.status}`
-          );
-        }
-
-        const data = await response.json();
-
-        if (data?.expired) {
-          message.error("Serial Expired!");
+    if(isOnline)
+    {
+      try {
+        const serialId = localStorage.getItem("lab-serial-id");
+        const exp = localStorage.getItem("lab-exp");
+        const labCreated = localStorage.getItem("lab-created");
+  
+        if (!exp || !labCreated || !serialId) {
+          console.warn("Required data missing for expiration check.");
           setIsLogin(false);
           setUser(null);
-        } else {
-          setIsLogin(true);
-          localStorage.setItem("lab-exp", data?.serial?.exp);
-          localStorage.setItem("lab-created", data?.serial?.startAt);
+          return;
         }
+  
+        const createdDate = dayjs(labCreated);
+        const today = dayjs();
+        const dayPassed = today.diff(createdDate, "day");
+  
+        // Define the expiration period based on the user type
+        const expirationPeriod = user?.type === "trial" ? 7 : 365;
+        const remaining = expirationPeriod - dayPassed;
+  
+  
+  
+        if (remaining <= 0) {
+          setIsLogin(false);
+          setUser(null);
+          message.error("Subscription has expired.");
+          return;
+        }
+  
+        if (user?.type !== "trial") {
+          const response = await fetch(
+            "https://dr-lab-apiv2.onrender.com/api/app/check-serial-expiration",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ serialId: parseInt(serialId, 10) }),
+            }
+          );
+  
+          if (!response.ok) {
+            throw new Error(
+              `Failed to check serial expiration, status code: ${response.status}`
+            );
+          }
+  
+          const data = await response.json();
+  
+          if (data?.expired) {
+            message.error("Serial Expired!");
+            setIsLogin(false);
+            setUser(null);
+          } else {
+            setIsLogin(true);
+            localStorage.setItem("lab-exp", data?.serial?.exp);
+            localStorage.setItem("lab-created", data?.serial?.startAt);
+          }
+        }
+      } catch (error) {
+        console.error(
+          "Error checking serial expiration:",
+          error.message || error
+        );
+        message.error("Failed to check serial expiration");
+        setIsLogin(false);
+        setUser(null);
       }
-    } catch (error) {
-      console.error(
-        "Error checking serial expiration:",
-        error.message || error
-      );
-      message.error("Failed to check serial expiration");
-      setIsLogin(false);
-      setUser(null);
+      return null;
+    };
     }
-  };
-
-  return null;
 };
 
 
